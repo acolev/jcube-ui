b02eca694f054ef2665e4f1b0a3cea9cb8610cec
Andrew Kolev <alteraweb53@gmail.com>
2024-02-28 17:21:30 +0200

    --

--- src/resources/views/layouts/auth.blade.php
+++ src/resources/views/layouts/auth.blade.php
@@ -1,13 +1,46 @@
 @extends('ui::layouts.master')
 
 @section('body')
-    <div class="auth-page-wrapper py-5 d-flex justify-content-center align-items-center min-vh-100">
-        <div class="auth-page-content overflow-hidden p-0">
-            <div class="auth-page-content">
-                <div class="container">
-                    @yield('content')
-                </div>
+    <div class="auth-page-wrapper pt-5">
+        <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
+            <div class="bg-overlay"></div>
+            <div class="shape">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
+                     viewBox="0 0 1440 120">
+                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+                </svg>
             </div>
         </div>
+        <div class="auth-page-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center mt-sm-5 mb-4 text-white-50">
+                            <div>
+                                <a href="/" class="d-inline-block auth-logo">
+                                    <x-ui-logo/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @yield('content')
+            </div>
+        </div>
+
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p class="mb-0 text-muted">&copy;
+                                <script>document.write(new Date().getFullYear())</script>
+                                {{__(gs()?->site_name ?: env('APP_NAME'))}}. @php echo env('APP_COPYRIGHT') @endphp
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 @endsection
