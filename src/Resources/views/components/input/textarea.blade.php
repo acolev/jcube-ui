08c1694418df3f02c2d71815621a23d97bcb55bb
Andrew Kolev <alteraweb53@gmail.com>
2024-02-29 10:21:00 +0200

    --

--- src/Resources/views/components/input/textarea.blade.php
+++ src/Resources/views/components/input/textarea.blade.php
@@ -9,10 +9,11 @@
 	"inline" => false,
 	"variants" => [],
 	"id" => null,
+	"class" => '',
 ])
 
 @if($label)
   <label class="@if(!!$required) required @endif" for="{{ $id }}">{!! __($label) !!}</label>
 @endif
-<textarea {{ $attributes }} class="form-control" name="{{ $name }}" @if(!!$required) required
+<textarea {{ $attributes }} @class(['form-control', $class]) name="{{ $name }}" @if(!!$required) required
           @endif id="{{ $id }}" {{ $attributes }}>@php echo $value @endphp</textarea>
