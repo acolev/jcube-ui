882374583d035daf1eb13c1565c429d66b457d5a
Andrew Kolev <alteraweb53@gmail.com>
2024-03-02 18:06:14 +0200

    --

--- src/Resources/js/notify.js
+++ src/Resources/js/notify.js
@@ -1,181 +1,124 @@
-class Notify {
-    messages = [];
-    items = [];
-    itemsToKill = [];
-    killTimeout = null;
-
-    set(val) {
-        this.messages.push(val)
-        this.spawnNotes();
+class NotificationManager {
+    constructor() {
+        this.queue = [];
+        this.activeNotifications = [];
+        this.pendingRemoval = [];
+        this.removalTimeout = null;
     }
 
-    spawnNote(i = 0) {
-        const id = i;
-        const trx = genTrx(4);
-        const message = this.messages[i];
-        const note = new Notification({
-            id: `note-${trx}`,
-            icon: message.icon,
-            title: message.title,
-            subtitle: message.subtitle,
-            actions: message.actions
+    push(message) {
+        this.queue.push(message);
+        this.displayNotifications();
+    }
+
+    displayNotification() {
+        if (!this.queue.length) return;
+
+        const message = this.queue.shift();
+        const transactionId = this.generateTransactionId(4);
+        const onClose = () => this.close(notification); // Замыкание для доступа к методу закрытия
+        const notification = new NotificationItem({
+            id: `notification-${transactionId}`,
+            ...message,
+            onClose // Передаем функцию закрытия в конструктор уведомления
         });
-        const transY = 100 * this.items.length;
 
-        note.el.style.transform = `translateY(${transY}%)`;
-        note.el.addEventListener("click", (evt) => this.dismiss(note, evt));
+        const translateY = 100 * this.activeNotifications.length;
+        notification.element.style.transform = `translateY(${translateY}%)`;
+        this.activeNotifications.push(notification);
 
-        this.items.push(note);
-        this.messages.splice(id, 1);
-        setTimeout(() => this.close(note), message.time || 30000)
-    }
-
-    spawnNotes(amount) {
-        let count = this.messages.length;
-
-        for (let i = 0; i < count; i++) {
-            this.spawnNote(i);
+        if (message.time) {
+            notification.autoCloseTimeout = setTimeout(onClose, message.time);
         }
     }
 
-    dismiss(note, e) {
-        const tar = e?.target;
-        if (("dismiss" in tar?.dataset)) {
-            this.close(note)
-        }
+    displayNotifications() {
+        this.queue.forEach(() => this.displayNotification());
     }
 
-    close(note) {
-        note.el.classList.add("notification--out");
-        this.itemsToKill.push(note);
-        clearTimeout(this.killTimeout);
+    close(notification) {
+        notification.element.classList.add("notification--out");
+        if (!this.pendingRemoval.includes(notification)) {
+            this.pendingRemoval.push(notification);
+        }
+        clearTimeout(notification.autoCloseTimeout);
 
-        this.killTimeout = setTimeout(() => {
-            this.itemsToKill.forEach(itemToKill => {
-                document.body.removeChild(itemToKill.el);
-
-                const left = this.items.filter(item => item.id !== itemToKill.id);
-                this.items = [...left];
+        this.removalTimeout = setTimeout(() => {
+            this.pendingRemoval.forEach(notificationToRemove => {
+                if (document.body.contains(notificationToRemove.element)) {
+                    document.body.removeChild(notificationToRemove.element);
+                }
+                this.activeNotifications = this.activeNotifications.filter(activeNotification => activeNotification.id !== notificationToRemove.id);
             });
 
-            this.itemsToKill = [];
-
-            if (!this.items.length)
-                this.spawnNotes();
-
-            this.shiftNotes();
-        }, note.killTime);
+            this.pendingRemoval = [];
+            this.adjustNotificationPositions();
+        }, 300);
     }
 
-    shiftNotes() {
-        this.items.forEach((item, i) => {
-            const transY = 100 * i;
-            item.el.style.transform = `translateY(${transY}%)`;
+    adjustNotificationPositions() {
+        this.activeNotifications.forEach((notification, index) => {
+            const translateY = 100 * index;
+            notification.element.style.transform = `translateY(${translateY}%)`;
         });
+    }
+
+    generateTransactionId(length) {
+        return Array.from({length}, () => Math.floor(Math.random() * 16).toString(16)).join('');
     }
 }
 
-class Notification {
-    constructor(args) {
-        this.args = args;
-        this.el = null;
-        this.id = null;
-        this.killTime = 300;
-        this.init(args);
+class NotificationItem {
+    constructor({id, icon, title, subtitle, actions, onClose}) {
+        this.id = id;
+        this.element = this.initialize({id, icon, title, subtitle, actions, onClose});
     }
 
-    init(args) {
-        const {id, icon, title, subtitle, actions} = args;
-        const block = "notification";
-        const parent = document.body;
-        const xmlnsSVG = "http://www.w3.org/2000/svg";
-        const xmlnsUse = "http://www.w3.org/1999/xlink";
+    initialize({id, icon, title, subtitle, actions, onClose}) {
+        const notificationElement = document.createElement('div');
+        notificationElement.id = id;
+        notificationElement.className = 'notification';
+        notificationElement.innerHTML = this.createHTML({icon, title, subtitle, actions});
 
-        const note = this.newEl("div");
-        note.id = id;
-        note.className = block;
-        parent.insertBefore(note, parent.lastElementChild);
-
-        const box = this.newEl("div");
-        box.className = `${block}__box`;
-        note.appendChild(box);
-
-        const content = this.newEl("div");
-        content.className = `${block}__content`;
-        box.appendChild(content);
-
-        const _icon = this.newEl("div");
-        _icon.className = `${block}__icon`;
-        content.appendChild(_icon);
-
-        const iconSVG = this.newEl("svg", xmlnsSVG);
-        iconSVG.setAttribute("class", `${block}__icon-svg`);
-        iconSVG.setAttribute("role", "img");
-        iconSVG.setAttribute("aria-label", icon);
-        iconSVG.setAttribute("width", "32px");
-        iconSVG.setAttribute("height", "32px");
-        _icon.appendChild(iconSVG);
-
-        const iconUse = this.newEl("use", xmlnsSVG);
-        iconUse.setAttributeNS(xmlnsUse, "href", `#${icon}`);
-        iconSVG.appendChild(iconUse);
-
-        const text = this.newEl("div");
-        text.className = `${block}__text`;
-        content.appendChild(text);
-
-        const _title = this.newEl("div");
-        _title.className = `${block}__text-title`;
-        _title.textContent = title;
-        text.appendChild(_title);
-
-        if (subtitle) {
-            const _subtitle = this.newEl("div");
-            _subtitle.className = `${block}__text-subtitle`;
-            _subtitle.textContent = subtitle;
-            text.appendChild(_subtitle);
+        const buttonsContainer = notificationElement.querySelector('.notification__btns');
+        if (typeof actions !== 'undefined') {
+            actions.forEach(action => {
+                const button = document.createElement('button');
+                button.className = 'notification__btn';
+                button.textContent = action.title || action;
+                if (typeof action === 'object' && action.cb) {
+                    button.addEventListener('click', () => action.cb(onClose));
+                } else {
+                    button.addEventListener('click', onClose);
+                }
+                buttonsContainer?.appendChild(button);
+            });
+        }else{
+            notificationElement.addEventListener('click', onClose);
         }
 
-        const btns = this.newEl("div");
-        btns.className = `${block}__btns`;
-        box.appendChild(btns);
-
-        actions.forEach(action => {
-            const btn = this.newEl("button");
-            let title = action
-            let cb = null
-            btn.className = `${block}__btn`;
-            btn.type = "button";
-
-            btn.setAttribute("data-dismiss", id);
-            if (typeof action !== 'string') {
-                title = action.title;
-                if (action.hasOwnProperty('cb')) {
-                    btn.addEventListener('click', action.cb);
-                } else if (action.hasOwnProperty('url')) {
-                    btn.addEventListener('click', () => location.href = action.url);
-                }
-
-            }
-
-            const btnText = this.newEl("span");
-            btnText.className = `${block}__btn-text`;
-            btnText.textContent = title;
-
-            btn.appendChild(btnText);
-            btns.appendChild(btn);
-        });
-
-        this.el = note;
-        this.id = note.id;
+        document.body.appendChild(notificationElement);
+        return notificationElement;
     }
 
-    newEl(elName, NSValue) {
-        if (NSValue)
-            return document.createElementNS(NSValue, elName);
-        else
-            return document.createElement(elName);
+    createHTML({icon, title, subtitle}) {
+        return `
+            <div class="notification__box">
+                <div class="notification__content">
+                    <div class="notification__icon">
+                        <svg class="notification__icon-svg" role="img" aria-label="${icon}" width="32px" height="32px">
+                            <use href="#${icon}"></use>
+                        </svg>
+                    </div>
+                    <div class="notification__text">
+                        <div class="notification__text-title">${title}</div>
+                        ${subtitle ? `<div class="notification__text-subtitle">${subtitle}</div>` : ''}
+                    </div>
+                </div>
+                <div class="notification__btns"></div>
+            </div>
+        `;
     }
 }
 
-window.notify = new Notify();
+window.notify = new NotificationManager();
