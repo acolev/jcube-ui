04ecd6e05a3b9b48babb73a799edb320056c9fb9
Andrew Kolev <alteraweb53@gmail.com>
2024-03-05 17:37:47 +0200

    --

--- src/Resources/views/components/input/text.blade.php
+++ src/Resources/views/components/input/text.blade.php
@@ -10,10 +10,21 @@
 	"variants" => [],
 	"id" => null,
 	"class" => '',
+	"groupTextPre" => null,
+	"groupTextPost" => null,
 ])
 
 @if($label)
     <label class="@if(!!$required) required @endif" for="{{ $id }}">{!! __($label) !!}</label>
 @endif
-<input @class(['form-control',$class]) type="{{ $attrType }}" name="{{ $name }}" @required(!!$required) value="{{ $value }}"
-       placeholder="{{ $placeholder }}" id="{{ $id }}" {{ $attributes }}>
+<div class="input-group">
+    @isset($groupTextPre)
+        <span class="input-group-text">{{ $groupTextPre }}</span>
+    @endisset
+    <input @class(['form-control',$class]) type="{{ $attrType }}" name="{{ $name }}"
+           @required(!!$required) value="{{ $value }}"
+           placeholder="{{ $placeholder }}" id="{{ $id }}" {{ $attributes }}>
+    @isset($groupTextPost)
+        <span class="input-group-text">{{ $groupTextPost }}</span>
+    @endisset
+</div>
